7bf6a902a6efee0a76a63d67b35b2437a1f31b62
qdozaq <doza@indiginis.com>
2018-12-16 23:58:37 -0500

    new vue version of site

---
+++ components/Social_Icons.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="social-icons">
+    <a href="mailto:paul@doza.design">
+      <img title="email" class="icon" src="/images/email.svg">
+    </a>
+    <a href="https://github.com/qdozaq" target="_blank" rel="noopener">
+      <img title="github" class="icon" src="/images/github.svg">
+    </a>
+    <a href="https://www.instagram.com/qdozaq/" target="_blank" rel="noopener">
+      <img title="instagram" class="icon" src="/images/instagram.svg">
+    </a>
+    <a href="https://www.linkedin.com/in/paul-mendoza-39303572/" target="_blank" rel="noopener">
+      <img title="linkedin" class="icon" src="/images/linkedin.svg">
+    </a>
+    <a href="https://www.youtube.com/user/qDozaq/" target="_blank" rel="noopener">
+      <img title="youtube" class="icon" src="/images/youtube.svg">
+    </a>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="container">
+  <logo/>
+  <h1>It me. Pauly D.</h1>
+  <div>
+    <p class="suplimental">This site is still under contruction
+    </p>
+    <p class="suplimental">
+      Here's some links:
+    </p>
+    <social/>
+  </div>
+</div>
+</template>
+
+<script>
+import Social from '~/components/Social_Icons.vue'
+
+export default {
+  components: {
+    Social
+  }
+}
+</script>
