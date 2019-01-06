731e014c569455c673b5263b56bb8811d8ab0514
qdozaq <doza@indiginis.com>
2019-01-05 20:31:52 -0500

    site in progress v2

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,23 @@
 <template>
-<div class="container">
-  <h1>It me. Pauly D.</h1>
-  <div>
-    <p class="suplimental">This site is still under contruction
-    </p>
-    <p class="suplimental">
-      Here's some links:
-    </p>
-    <social/>
+  <div class="container">
+    <demo/>
+    <h1 id="introduction">I'm Paul Mendoza</h1>
+    <div>
+      <p class="suplimental">This site is still under contruction</p>
+      <p class="suplimental">Here's some links:</p>
+      <social/>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import Social from '~/components/Social_Icons.vue'
+import Social from "~/components/Social_Icons.vue";
+import Demo from "~/components/Demo_Reel.vue";
 
 export default {
   components: {
-    Social
+    Social,
+    Demo
   }
-}
+};
 </script>
