a51b97d8bfc2f043cbf34db9767732b4de504e1d
qdozaq <doza@indiginis.com>
2020-08-20 22:46:49 -0400

    grid stuff

--- src/components/HomeSection.svelte
+++ src/components/HomeSection.svelte
@@ -5,18 +5,41 @@
 <style>
   section {
     position: absolute;
-    top: 50%;
-    margin: 0 2rem;
+    display: grid;
+    /* top: 50%;
+    left: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%); */
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(6, 1fr);
   }
 
   h1 {
+    z-index: 2;
     font-weight: 500;
     font-size: 48px;
     line-height: 56px;
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.4);
+    grid-row: 1 / 3;
+    grid-column: 2 / 4;
   }
+
+  .content {
+    grid-row: 2 / 5;
+    grid-column: 3 / 6;
+  }
+
+  /* .action {
+    grid-row: 4 / 5;
+  } */
 </style>
 
 <section>
+  <div class="content">
+    <slot name="content" />
+    <div class="action">
+      <slot name="action" />
+    </div>
+  </div>
   <h1>{title}</h1>
-
 </section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-	import HomeSection from 'components/HomeSection.svelte';
+  import HomeSection from "components/HomeSection.svelte";
 </script>
 
 <style>
-	:global(body) {
-		margin: 0;
-		background-color: #111;
-		font-family: Roboto, sans-serif;
-		color: #fff;
-	}
+  :global(body) {
+    margin: 0;
+    background-color: #111;
+    font-family: Roboto, sans-serif;
+    color: #fff;
+  }
 
+  img {
+    width: 212px;
+  }
 </style>
 
 <svelte:head>
-	<title>I'm Paul Mendoza</title>
+  <title>I'm Paul Mendoza</title>
 </svelte:head>
 
-<main>	
-	<HomeSection title="Paul Mendoza"/>
+<main>
+  <HomeSection title="Paul Mendoza">
+    <img alt="evening" src="/evening.jpg" slot="content" />
+    <a slot="action">more</a>
+  </HomeSection>
 </main>
