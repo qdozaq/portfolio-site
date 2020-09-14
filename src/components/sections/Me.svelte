459ec88156bfa4b15672684e525b492e04cf89d0
Paul Mendoza <doza@indiginis.com>
2020-09-14 00:59:40 -0400

    image optimization

--- src/components/sections/Me.svelte
+++ src/components/sections/Me.svelte
@@ -5,13 +5,26 @@
 
 <style>
   img {
-    width: inherit;
+    width: 14rem;
     box-shadow: 0px 4px 20px 1px rgba(0, 0, 0, 0.5);
+  }
+
+  @media only screen and (min-width: 40rem) {
+    img {
+      width: 20rem;
+      height: auto;
+    }
   }
 </style>
 
 <Section title="Paul Mendoza" {...$$props}>
-  <img alt="me" src="/me.jpg" slot="content" />
+  <img
+    alt="me"
+    src="/me.jpg"
+    width="14rem"
+    height="296.18"
+    sizes="(min-width: 40rem) 20rem, 14rem"
+    slot="content" />
   <span slot="action">
     <NavigationButton href="/about">about</NavigationButton>
   </span>
