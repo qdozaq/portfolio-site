0cb8cbdd65e5103fa75dc15db64003f57f09b4ad
Paul Mendoza <doza@indiginis.com>
2020-10-01 00:35:56 -0400

    start experience page

---
+++ src/components/PageSection.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let heading: string = "";
+  export let subHeading: string = "";
+  export let detail: string = "";
+</script>
+
+<style>
+  h2 {
+    font-weight: 500;
+  }
+
+  h3 {
+    font-style: italic;
+    font-weight: 400;
+  }
+
+  h4 {
+    font-weight: 100;
+  }
+</style>
+
+<h2>{heading}</h2>
+
+<h3>{subHeading}</h3>
+
+<h4>{detail}</h4>
+
+<slot />
--- src/routes/experience.svelte
+++ src/routes/experience.svelte
@@ -1,5 +1,6 @@
 <script>
   import NavigationButton from "components/NavigationButton.svelte";
+  import PageSection from "components/PageSection.svelte";
 </script>
 
 <style>
@@ -9,10 +10,17 @@
   }
 
   p {
-    font-size: 1.2rem;
+    font-weight: 300;
+    font-size: 1rem;
   }
-  div {
-    width: 7rem;
+
+  hr {
+    border-top: none;
+    border-color: #aaa;
+    max-width: 30rem;
+    text-align: left;
+    margin-left: 0;
+    margin-bottom: 2rem;
   }
 </style>
 
@@ -20,7 +28,45 @@
   <title>Experience</title>
 </svelte:head>
 
-<div>
-  <NavigationButton href="/" point="left">Back</NavigationButton>
-</div>
 <h1>Experience</h1>
+
+<PageSection
+  heading="Vimeo"
+  subHeading="Senior Frontend Platform Engineer"
+  detail="Mar 2019 — Current">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu eleifend
+    elit, ut porttitor diam. Nullam porta velit neque, sit amet feugiat arcu
+    blandit sit amet. Nulla enim turpis, ornare at pulvinar sed, tempor eu enim.
+  </p>
+</PageSection>
+<PageSection
+  heading="Fidelity"
+  subHeading="Software Engineer"
+  detail="Sep 2017 — Mar 2019">
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu eleifend
+    elit, ut porttitor diam. Nullam porta velit neque, sit amet feugiat arcu
+    blandit sit amet. Nulla enim turpis, ornare at pulvinar sed, tempor eu enim.
+  </p>
+</PageSection>
+
+<hr />
+
+<h1>Skills</h1>
+<PageSection heading="Languages" />
+<PageSection heading="Frameworks/Livraries" />
+<PageSection heading="Tool/Applications" />
+
+<hr />
+
+<h1>Education</h1>
+
+<PageSection
+  heading="The University of North Carolina at Chapel Hill"
+  subHeading="Bachelor of Arts in Computer Science"
+  detail="May 2017" />
+<PageSection
+  heading="Rowan Cabarrus Community College"
+  subHeading="Associates in Science"
+  detail="May 2015" />
