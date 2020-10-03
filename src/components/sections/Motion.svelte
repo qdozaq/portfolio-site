c7564a6186a1279272765a4a40a3fed181027cfd
Paul Mendoza <doza@indiginis.com>
2020-10-03 00:33:36 -0400

    add back webm

--- src/components/sections/Motion.svelte
+++ src/components/sections/Motion.svelte
@@ -12,7 +12,7 @@
 
 <Section title="Motion Designer" {...$$props}>
   <video playsinline autoplay muted loop slot="content">
-    <!-- <source src="/motion_preview.webm" type="video/webm" /> -->
+    <source src="/motion_preview.webm" type="video/webm" />
     <source src="/motion_preview.mp4" type="video/mp4" />
   </video>
   <span slot="action">
